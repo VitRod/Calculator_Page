ce36e3c55f3723ee929acce0513e861465120f5a
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-24 07:16:36 +0300

    // calculate first and second values dependind on operator const calculate = {   '/': (firstNumber, secondNumber) => firstNumber / secondNumber,   '*': (firstNumber, secondNumber) => firstNumber * secondNumber,   '+': (firstNumber, secondNumber) => firstNumber + secondNumber,   '-': (firstNumber, secondNumber) => firstNumber - secondNumber,   '=': (firstNumber, secondNumber) => secondNumber, };

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
-
+// calculate first and second values dependind on operator
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber,
+  };
 
 
 
